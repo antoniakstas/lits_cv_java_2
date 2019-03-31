7931f4085bdee28eab5536c0892e4a1fdd8f4329
Tetiana <tudantan13@gmail.com>
2019-03-31 20:00:00 +0300

    product

--- src/dto/Product.java
+++ src/dto/Product.java
@@ -1,4 +1,71 @@
 package dto;
 
 public class Product {
+
+    private String code;
+    private String name;
+    private String manufacturer;
+    private int count;
+
+
+    public  Product(){
+
+    }
+    public Product(String name , String manufacturer, String code, int count) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.code = code;
+        this.count = count;
+    }
+
+    public Product(String name, String manufacturer, String code) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.code = code;
+    }
+
+
+    public String getNameautoparts() {
+        return name;
+    }
+
+    public void setNameautoparts(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getQuantity() {
+        return count;
+    }
+
+    public void setQuantity(int count) {
+        this.count = count;
+    }
+
+
+    @Override
+    public String toString() { return "|" + name + "| "+ manufacturer + "| "+ code + "| " + count + "|\n";}
+
+
+
+
+
 }
+
+
+
