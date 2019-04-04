6cdd6b7f95783e1ee2a8ec525c486491cc758685
Ilia <ilia97ap76@gmail.com>
2019-04-04 14:43:48 +0300

    changes to the Price class

--- src/launcher/PriceLauncher.java
+++ src/launcher/PriceLauncher.java
@@ -5,10 +5,10 @@ public class PriceLauncher {
     public static final String DB_URL = "jdbc:mysql://db4free.net:3306/lits_cv_java_2";
     public static final String DB_USER = "java_2_user";
     public static final String DB_PASSWORD = "java_2_password";
-    public static final String DB_COLUMN_PRODUCT_ID = "id";
-    public static final String DB_COLUMN_PRODUCT_PRODUCT_ID = "product_id";
-    public static final String DB_COLUMN_PRODUCT_VALUE = "value";
-    public static final String DB_COLUMN_PRODUCT_MULT = "mult";
-    public static final String DB_COLUMN_PRODUCT_ACTIVE = "active";
-    public static final String DB_COLUMN_PRODUCT_DELIVERYDAYS = "deliverydays";
+    public static final String DB_COLUMN_PRICE_ID = "id";
+    public static final String DB_COLUMN_PRICE_PRODUCT_ID = "product_id";
+    public static final String DB_COLUMN_PRICE_VALUE = "value";
+    public static final String DB_COLUMN_PRICE_MULT = "mult";
+    public static final String DB_COLUMN_PRICE_ACTIVE = "active";
+    public static final String DB_COLUMN_PRICE_DELIVERYDAYS = "deliverydays";
 }
