d10bc2dca8461d03ac389984a2908b89973dd406
Tetiana <tudantan13@gmail.com>
2019-04-07 11:38:43 +0300

    good select for productlauncher

--- src/launcher/ProductLauncher.java
+++ src/launcher/ProductLauncher.java
@@ -12,7 +12,7 @@ public class ProductLauncher {
     public static final String DB_USER = "java_2_user";
     public static final String DB_PASSWORD = "java_2_password";
     public static final String DB_COLUMN_PRODUCT_ID = "id";
-//    public static final String DB_COLUMN_PRODUCT_INDEX = "index";
+    public static final String DB_COLUMN_PRODUCT_INDEX = "index";
     public static final String DB_COLUMN_PRODUCT_NAME = "name";
     public static final String DB_COLUMN_PRODUCT_MANUFACTURER = "manufacturer";
     public static final String DB_COLUMN_PRODUCT_COUNT = "count";
@@ -46,7 +46,7 @@ public static void main(String[] args){
     }
 
 
-    public static void selectProduct(){
+    public static void selectProduct() {
 
 
         List<Product> productList = new ArrayList<>();
@@ -57,35 +57,39 @@ public static void main(String[] args){
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
             statement = connection.createStatement();
 
-            String sqlQuery = "SELECT "+DB_COLUMN_PRODUCT_ID+", name FROM Product";
+            String sqlQuery = "SELECT "+DB_COLUMN_PRODUCT_ID+", `"+DB_COLUMN_PRODUCT_INDEX+"`, "+DB_COLUMN_PRODUCT_NAME+"," +
+                    ""+DB_COLUMN_PRODUCT_MANUFACTURER+", "+DB_COLUMN_PRODUCT_COUNT+" FROM Product";
+//            String sqlQuery = "SELECT  id,  `index`,   name,   manufacturer, count FROM Product";
             ResultSet resultSet = statement.executeQuery(sqlQuery);
 
             while (resultSet.next()) {
                 Integer idValueFromDB = resultSet.getInt(DB_COLUMN_PRODUCT_ID);
-//                String indexValueFromDB = resultSet.getString(DB_COLUMN_PRODUCT_INDEX);
+                String indexValueFromDB = resultSet.getString(DB_COLUMN_PRODUCT_INDEX);
                 String nameValueFromDB = resultSet.getString(DB_COLUMN_PRODUCT_NAME);
                 String manufacturerValueFromDB = resultSet.getString(DB_COLUMN_PRODUCT_MANUFACTURER);
                 Integer countValueFromDB = resultSet.getInt(DB_COLUMN_PRODUCT_COUNT);
 
-                Product productFromDB = new Product(idValueFromDB, nameValueFromDB, manufacturerValueFromDB, countValueFromDB);
+                Product productFromDB = new Product(idValueFromDB, indexValueFromDB, nameValueFromDB, manufacturerValueFromDB, countValueFromDB);
 
                 productList.add(productFromDB);
 
-            }
+                String resultString = "Next values from the DB: id = " +
+                        idValueFromDB + ", index = " + indexValueFromDB + ", name = " + nameValueFromDB +
+                        ", manufacturer = " + manufacturerValueFromDB
+                        + ", count = " + countValueFromDB;
 
-        } catch (SQLException e) {
+                System.out.println(resultString);
+
+
+//                for (Product productItem:
+//                        productList) {
+//                    System.out.println(productItem.toString());
+//                }
+        }
+        } catch(SQLException e){
             e.printStackTrace();
         }
-
-        for (Product productItem:productList) {
-            System.out.println(productItem.toString());
-        }
     }
-
-
-
-
-
 }
 
 
