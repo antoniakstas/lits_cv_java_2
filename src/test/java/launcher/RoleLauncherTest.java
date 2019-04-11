b58580c986af26172f9be8cc4bb573ed61ba3ef5
Marko Pyrizhok <mark.pyrizhok@gmail.com>
2019-04-11 20:17:46 +0300

    remove test code

--- src/test/java/launcher/RoleLauncherTest.java
+++ src/test/java/launcher/RoleLauncherTest.java
@@ -1,27 +1,5 @@
 package launcher;
 
-//import dto.Role;
-import org.junit.Test;
-
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
-
 public class RoleLauncherTest {
 
-	@Test
-	public void testReadFromDB(){
-		assertTrue(true);
-
-	}
-
-	@Test
-	public void testCorrectSelectStatement(){
-//		assertEquals(Role.getSelectSqlQuery(), "SELECT * FROM role;");
-	}
-
-
-	@Test
-	public void testCorrectReturnOnSelectStatement(){
-//		assertEquals(Role.getSelectSqlQuery(), "SELECT * FROM role;");
-	}
 }
