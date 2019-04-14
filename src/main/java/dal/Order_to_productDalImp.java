19fd491dd86910a7b29b0185957b08c966a600dd
Melnichuk Olexandr <alex.melnichuk90@gmail.com>
2019-04-14 22:05:47 +0300

    Order_to_product3

--- src/main/java/dal/Order_to_productDalImp.java
+++ src/main/java/dal/Order_to_productDalImp.java
@@ -6,13 +6,14 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class Order_to_productDalImp implements Order_to_productDal {
 
     public static final String DB_URL = "jdbc:mysql://db4free.net:3306/lits_cv_java_2";
     public static final String DB_USER = "java_2_user";
     public static final String DB_PASSWORD = "java_2_password";
-    //public static final String DB_COLUMN_USER_ID = "id";
+    public static final String DB_COLUMN_USER_ID = "id";
     public static final String DB_TABLE_ORDER_TO_PRODUCT = "order_to_product";
     public static final String DB_ID = "id";
     public static final String DB_ORDER_ID = "order_" + DB_ID;
@@ -22,7 +23,7 @@ public class Order_to_productDalImp implements Order_to_productDal {
 
     public Order_to_productDalImp() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -37,11 +38,32 @@ public class Order_to_productDalImp implements Order_to_productDal {
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
             statement = connection.createStatement();
 
-            String sqlQuery = "SELECT * FROM order_to_productList";
+        /*    Scanner scanner = new Scanner(System.in);
+            System.out.println("Input a number: ");
+            int num = scanner.nextInt();
+            if (num==1){
+                String insert_intoTableSQL = "INSERT INTO `lits_cv_java_2`.`" + DB_TABLE_ORDER_TO_PRODUCT + "` " +
+                        "(`" + DB_ID + "`, `" + DB_ORDER_ID + "`, `" + DB_PRODUCT_COUNT + "`, `" + DB_PRICE_ID + "`) VALUES ('11', '4', '6', '8')";
+                statement.execute(insert_intoTableSQL);
+            }
+            if (num==2){
+                String updateTableSQL = "UPDATE `lits_cv_java_2`." +
+                        "`" + DB_TABLE_ORDER_TO_PRODUCT + "` SET `" + DB_ORDER_ID + "` = '6', " +
+                        "`" + DB_PRODUCT_COUNT + "` = '5', `" + DB_PRICE_ID + "` = '6' WHERE (`" + DB_ID + "` = '3')";
+                statement.execute(updateTableSQL);
+            }
+            if (num==3){
+                String deleteTableSQL = "DELETE FROM `lits_cv_java_2`.`" + DB_TABLE_ORDER_TO_PRODUCT + "` WHERE (`" + DB_ID + "` = '11')";
+                statement.execute(deleteTableSQL);
+            }
+            scanner.close();*/
+            String sqlQuery = "SELECT " + DB_ID + ", " + DB_ORDER_ID + ", " + DB_PRODUCT_COUNT + ", " + DB_PRICE_ID + " FROM " + DB_TABLE_ORDER_TO_PRODUCT;
+           // String sqlQuery = "SELECT * FROM user";
             ResultSet resultSet = statement.executeQuery(sqlQuery);
 
+
             while (resultSet.next()) {
-                Integer idValueFromDB = resultSet.getInt(DB_ID);
+                Integer idValueFromDB = resultSet.getInt(DB_COLUMN_USER_ID);
                 Integer order_idValueFromDB = resultSet.getInt(DB_ORDER_ID);
                 Integer product_countValueFromDB = resultSet.getInt(DB_PRODUCT_COUNT);
                 Integer price_idValueFromDB = resultSet.getInt(DB_PRICE_ID);
