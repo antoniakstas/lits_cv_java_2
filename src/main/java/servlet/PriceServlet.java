ef93dbc7f784914ab8dd57985f35f66cc928424f
Ilia <ilia97ap76@gmail.com>
2019-04-29 16:28:07 +0300

    add priceModel

--- src/main/java/servlet/PriceServlet.java
+++ src/main/java/servlet/PriceServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import dal.PriceDalImp;
-
 import dto.Price;
 
 import javax.servlet.ServletOutputStream;
@@ -9,31 +8,60 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class PriceServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) {
-
-        System.out.println("here is");
-
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) {
         ServletOutputStream out = null;
+
+
+        // http://localhost:8880/application/user?action=create&id=5
+
+        String parameterAct = request.getParameter("action");
+        String parameterId1 = request.getParameter("id");
+
+        String uri = request.getScheme() +
+                "://" +
+                request.getServerName() +
+                ":" +
+                request.getServerPort() +
+                request.getRequestURI() +
+                "?" +
+                request.getQueryString();
+
         try {
             out = response.getOutputStream();
             out.println("<html>");
             out.println("<head><title>Hello Servlet</title></head>");
             out.println("<body>");
-            out.println("<h3>Hello World</h3>");
+            out.println("<h3>Hello Price</h3>");
+            out.println(uri);
+            out.println("<br>");
+            out.println(parameterId1 + " was passed as the id");
+            out.println("<br>");
+            out.println(parameterAct + " was passed as the action");
+
 
             PriceDalImp priceDal = new PriceDalImp();
-            Price price = priceDal.readAllFromDB().get(0);
-            out.println(price.toString());
+            List<Price> priceList = priceDal.readAllFromDB();
 
+            out.println("<ul>");
+            for (Price priceItem : priceList) {
+                out.println("<li>" + priceItem.toString() + "</li>");
+            }
+
+            out.println("</ul>");
             out.println("This is my first Servlet");
             out.println("</body>");
             out.println("<html>");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
+
 }
