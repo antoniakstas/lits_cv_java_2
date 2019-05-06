020096621d097fb9a1fbfc2c69713baaf21ff3b1
Marko Pyrizhok <mark.pyrizhok@gmail.com>
2019-05-06 20:43:20 +0300

    add template resolver to spring context

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,9 +2,9 @@ package controller;
 
 
 import model.UserModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/user")
@@ -15,5 +15,32 @@ public class UserController {
         System.out.println("I'm in the GET method!");
         UserModel userModel = new UserModel(123, " user name", "url");
         return userModel;
+    }
+
+
+    @GetMapping(path = "/listpage")
+    public ModelAndView findAllPage() {
+        System.out.println("I'm in the GET method!");
+        UserModel userModel = new UserModel(123, " user name", "url");
+        ModelAndView model = new ModelAndView("testPage");
+        model.addObject("msg", "some message from Controller");
+
+        return model;
+    }
+
+    @GetMapping("/listpage/create")
+    public ModelAndView greetingForm(Model model) {
+        UserModel userModel = new UserModel();
+        userModel.setName("predefined name");
+        model.addAttribute("userModel", userModel);
+
+        ModelAndView modelAndView = new ModelAndView("createUser", "userModel", userModel);
+
+        return modelAndView;
+    }
+
+    @PostMapping("/listpage/result")
+    public String greetingSubmit(@ModelAttribute UserModel userModel) {
+        return "createUserResult";
     }
 }
