eee055421bc07031522082eaf2f85ca8dabb8b5d
Marko Pyrizhok <mark.pyrizhok@gmail.com>
2019-05-10 21:00:05 +0300

    change user controller

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,20 +1,26 @@
 package controller;
 
 
+import dal.UserDalImp;
+import dto.User;
 import model.UserModel;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @GetMapping(path = "/list")
-    public UserModel findAll() {
+    public List<User> findAll() {
         System.out.println("I'm in the GET method!");
         UserModel userModel = new UserModel(123, " user name", "url");
-        return userModel;
+        UserDalImp dal = new UserDalImp();
+        dal.readAllFromDB();
+        return dal.readAllFromDB();
     }
 
 
