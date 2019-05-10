1bfedbe274f85976882d0c717d8000ebd2ebb40d
Marko Pyrizhok <mark.pyrizhok@gmail.com>
2019-05-10 20:24:21 +0300

    check dbforfree db

--- src/main/java/dal/PriceDalImp.java
+++ src/main/java/dal/PriceDalImp.java
@@ -11,9 +11,9 @@ import java.util.Optional;
 
 public class PriceDalImp implements PriceDal {
 
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/lits_cv_java_2?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "root";
+    public static final String DB_URL = "jdbc:mysql://db4free.net:3306/lits_cv_java_2";
+    public static final String DB_USER = "java_2_user";
+    public static final String DB_PASSWORD = "java_2_password";
     public static final String DB_COLUMN_PRICE_ID = "id";
     public static final String DB_COLUMN_PRICE_PRODUCT_ID = "product_id";
     public static final String DB_COLUMN_PRICE_VALUE = "value";
