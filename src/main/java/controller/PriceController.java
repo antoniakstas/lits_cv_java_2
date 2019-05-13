287ed49bfbd14b81750d4583b9eba6e9c9bfb5e0
Ilia <ilia97ap76@gmail.com>
2019-05-13 21:21:34 +0300

    add PriceListResponseModel and PriceResponseModel

--- src/main/java/controller/PriceController.java
+++ src/main/java/controller/PriceController.java
@@ -2,29 +2,68 @@ package controller;
 
 import dal.PriceDalImp;
 import dto.Price;
+import model.PriceListResponceModel;
 import model.PriceModel;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import model.PriceResponceModel;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/price")
 public class PriceController {
 
+//    @GetMapping(path = "/list")
+//    public List<Price> findAll() {
+//        System.out.println("I'm in the GET method!");
+//        PriceModel priceModel = new PriceModel(1, 123, 43,2,"active", "4 days", "url");
+//        PriceDalImp dal = new PriceDalImp();
+//        return dal.readAllFromDB();
+//    }
+
     @GetMapping(path = "/list")
-    public List<Price> findAll() {
-        System.out.println("I'm in the GET method!");
-        PriceModel priceModel = new PriceModel(1, 123, 43,2,"active", "4 days", "url");
-        PriceDalImp dal = new PriceDalImp();
-        dal.readAllFromDB();
-        return dal.readAllFromDB();
+    public PriceListResponceModel findAll(){
+
+        PriceListResponceModel responceModel = new PriceListResponceModel();
+
+        PriceListResponceModel element1 = new PriceListResponceModel();
+        PriceListResponceModel element2 = new PriceListResponceModel();
+
+        List<PriceListResponceModel> priceListResponceModels = new ArrayList<>();
+
+        priceListResponceModels.add(element1);
+        priceListResponceModels.add(element2);
+
+        responceModel.setPriceListResponceModels(priceListResponceModels);
+
+        return responceModel;
+
+    }
+    @GetMapping(path = "/item")
+    public PriceResponceModel findItem(Integer id){
+
+        PriceResponceModel responceModel = new PriceResponceModel();
+
+
+
+        return responceModel;
+
     }
 
-//
+    @PostMapping(path = "/item")
+    public PriceResponceModel createItem(@RequestBody PriceResponceModel model){
+
+        PriceResponceModel responceModel = new PriceResponceModel();
+
+
+
+        return responceModel;
+
+    }
 //    @GetMapping(path = "/listpage")
 //    public ModelAndView findAllPage() {
 //        System.out.println("I'm in the GET method!");
---
+++ src/main/java/model/PriceResponceModel.java
@@ -0,0 +1,58 @@
+package model;
+
+public class PriceResponceModel {
+    private int id;
+    private int produkt_id;
+    private int value;
+    private int mult;
+    private String active;
+    private String deliverydays;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProdukt_id() {
+        return produkt_id;
+    }
+
+    public void setProdukt_id(int produkt_id) {
+        this.produkt_id = produkt_id;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getMult() {
+        return mult;
+    }
+
+    public void setMult(int mult) {
+        this.mult = mult;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+
+    public String getDeliverydays() {
+        return deliverydays;
+    }
+
+    public void setDeliverydays(String deliverydays) {
+        this.deliverydays = deliverydays;
+    }
+}
