1b66194c6d93c5f9d683b77cf0f93ec7da5588c8
Ilia <ilia97ap76@gmail.com>
2019-05-13 15:29:08 +0300

    add OrderDal and OrderDalImpl

---
+++ src/main/java/dal/OrderDalImpl.java
@@ -0,0 +1,106 @@
+
+package dal;
+
+import dto.Order;
+import dto.Price;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+    public class OrderDalImpl implements OrderDal {
+
+        public static final String DB_URL = "jdbc:mysql://localhost:3306/lits_cv_java_2?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        public static final String DB_USER = "root";
+        public static final String DB_PASSWORD = "root";
+        public static final String DB_COLUMN_ORDER_ID = "id";
+        public static final String DB_COLUMN_STATUS = "status";
+        public static final String DB_COLUMN_USER_M_ID = "user_m_id";
+        public static final String DB_COLUMN_USER_C_ID = "user_c_id";
+        public static final String DB_TABLE_ORDER = "order";
+
+
+        public OrderDalImpl() {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public List<Order> readAllFromDB() {
+            Connection connection = null;
+            Statement statement = null;
+            List<Order> orderList = new ArrayList<>();
+            try {
+                connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                statement = connection.createStatement();
+
+                String sqlQuery = "SELECT * FROM "+DB_TABLE_ORDER+"";
+                ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+                while (resultSet.next()) {
+                    Integer id = resultSet.getInt(DB_COLUMN_ORDER_ID);
+                    String status = resultSet.getString(DB_COLUMN_STATUS);
+                    Integer user_m_id = resultSet.getInt(DB_COLUMN_USER_M_ID);
+                    Integer user_c_id = resultSet.getInt(DB_COLUMN_USER_C_ID);
+
+                    Order order = new Order(id, status, user_m_id, user_c_id );
+                    orderList.add(order);
+
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return orderList;
+        }
+
+        @Override
+        public Optional<Order> readFromDBById(int id) {
+            return Optional.empty();
+        }
+
+
+        @Override
+        public boolean updateOrder(int id, Order order) {
+            return false;
+        }
+
+        @Override
+        public boolean deleteOrder(int id) {
+            return false;
+        }
+
+        @Override
+        public boolean createOrderInToDB(Order order) {
+            try {
+                Connection connection = null;
+                Statement statement = null;
+                connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                statement = connection.createStatement();
+                //         String insertTableSQL = INSERT INTO `lits_cv_java_2`.`price` (`product_id`, `value`, `mult`, `active`, `deliverydays`)
+                //         VALUES ('2', '2', '3', 'active', '4 days');
+
+                String insertTableSQL = "INSERT INTO " + DB_TABLE_ORDER
+                        + "("+DB_COLUMN_ORDER_ID+", "+DB_COLUMN_STATUS+", "+DB_COLUMN_USER_M_ID+", "+DB_COLUMN_USER_C_ID+" ) " + "VALUES"
+                        + "("+order.getId()+",'"+order.getStatus()+"','"+order.getUser_m_id()+"','"+order.getUser_c_id()+"' )";
+                statement.executeUpdate(insertTableSQL);
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+
+            return false;
+
+        }
+
+
+    }
+
+
+
