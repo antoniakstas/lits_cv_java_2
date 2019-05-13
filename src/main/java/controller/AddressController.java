c16a8f797609499142b652f47b1c8815a9211e2e
Marko Pyrizhok <mark.pyrizhok@gmail.com>
2019-05-13 21:20:49 +0300

    add address item, list, create apis

--- src/main/java/controller/AddressController.java
+++ src/main/java/controller/AddressController.java
@@ -1,18 +1,34 @@
 package controller;
 
+import model.AddressListResponseModel;
+import model.AddressResponseModel;
 import model.UserModel;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/address")
 class AddressController {
 
     @GetMapping(path = "/list")
-    public UserModel findAll() {
-        System.out.println("I'm in the GET method!");
-        UserModel userModel = new UserModel(123, " user name", "url");
-        return userModel;
+    public AddressListResponseModel findAll() {
+        AddressListResponseModel responseModel = new AddressListResponseModel();
+
+        AddressResponseModel element1 = new AddressResponseModel();
+        AddressResponseModel element2 = new AddressResponseModel();
+        List<AddressResponseModel> addressResponseModels = new ArrayList<>();
+
+        addressResponseModels.add(element1);
+        addressResponseModels.add(element2);
+
+        responseModel.setAddressResponseModels(addressResponseModels);
+
+        return responseModel;
     }
+
 
     @PostMapping(path = "/create")
     @ResponseBody
@@ -22,5 +38,23 @@ class AddressController {
 
         return new UserModel(123, "anotherName", "urlFromPost");
     }
+
+
+    @GetMapping(path = "/item")
+    public AddressResponseModel findItem(Integer id) {
+        AddressResponseModel responseModel = new AddressResponseModel();
+
+
+        return responseModel;
+    }
+
+    @PostMapping(path = "/item")
+    public AddressResponseModel createItem(@RequestBody AddressResponseModel model) {
+        AddressResponseModel responseModel = new AddressResponseModel();
+
+
+        return responseModel;
+    }
+
 
 }
