ad4eb8ab11a46a361ab59c1e407206c826400317
Ilia <ilia97ap76@gmail.com>
2019-05-13 15:10:17 +0300

    add OrderModel

---
+++ src/main/java/model/OrderModel.java
@@ -0,0 +1,57 @@
+package model;
+
+public class OrderModel {
+    private int id;
+    private String status;
+    private int user_m_id;
+    private int user_c_id;
+
+    @Override
+    public String toString() {
+        return "OrderModel{" +
+                "id=" + id +
+                ", status='" + status + '\'' +
+                ", user_m_id=" + user_m_id +
+                ", user_c_id=" + user_c_id +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getUser_m_id() {
+        return user_m_id;
+    }
+
+    public void setUser_m_id(int user_m_id) {
+        this.user_m_id = user_m_id;
+    }
+
+    public int getUser_c_id() {
+        return user_c_id;
+    }
+
+    public void setUser_c_id(int user_c_id) {
+        this.user_c_id = user_c_id;
+    }
+
+    public OrderModel(int id, String status, int user_m_id, int user_c_id) {
+        this.id = id;
+        this.status = status;
+        this.user_m_id = user_m_id;
+        this.user_c_id = user_c_id;
+    }
+}
