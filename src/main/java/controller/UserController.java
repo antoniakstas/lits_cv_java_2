de0400301fa820d0cdc0486d750be7d9cb39b41e
Marko Pyrizhok <mark.pyrizhok@gmail.com>
2019-05-27 20:24:23 +0300

    add interceptors examples

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Locale;
@@ -66,7 +68,9 @@ public class UserController {
     }
 
     @PostMapping("/registration")
-    public UserRegistrationResponse registration(Locale locale, @Valid @RequestBody UserRegistrationRequest request, BindingResult bindingResult) {
+    public UserRegistrationResponse registration(HttpServletRequest httpRequest,
+                                                 HttpServletResponse httpResponse,
+                                                 Locale locale, @Valid @RequestBody UserRegistrationRequest request, BindingResult bindingResult) {
 
         UserRegistrationResponse response = new UserRegistrationResponse();
 
