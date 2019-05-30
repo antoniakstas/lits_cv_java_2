d06ceb9a33123f6e99bec900d4850b2019bfbcd1
Ilia <ilia97ap76@gmail.com>
2019-05-30 12:33:38 +0300

    add valid on PriceResponseModel

--- src/main/java/model/PriceResponceModel.java
+++ src/main/java/model/PriceResponceModel.java
@@ -1,17 +1,46 @@
 package model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class PriceResponceModel {
 
-    @NotNull
-    @Size
+    @NotNull(message = "Please provide id")
+    @Size(min=3, max=50)
+    @ApiModelProperty(notes = "id of the price")
+
     private int id;
+
+    @NotNull(message = "Please provide product_id")
+    @Size(min=3, max=50)
+    @ApiModelProperty(notes = "product_id of the price")
+
     private int product_id;
+
+    @NotNull(message = "Please provide value")
+    @Size(min=3, max=50)
+    @ApiModelProperty(notes = "value of the price")
+
     private int value;
+
+    @NotNull(message = "Please provide mult")
+    @Size(min=2, max=12)
+    @ApiModelProperty(notes = "mult of the price")
+
     private int mult;
+
+    @NotNull(message = "Please provide active")
+    @Size(min=3, max=10)
+    @ApiModelProperty(notes = "active of the price")
+
     private String active;
+
+    @NotNull(message = "Please provide deliverydays")
+    @Size(min=2, max=7)
+    @ApiModelProperty(notes = "deliverydays of the price")
+
     private String deliverydays;
 
     public int getId() {
