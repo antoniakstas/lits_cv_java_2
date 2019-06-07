69eca4fcfcf48d6e8993eced3f49414aa203e6b5
Ilia <ilia97ap76@gmail.com>
2019-06-07 11:16:12 +0300

    add User to webmvc

--- src/main/java/dal/UserDalImp.java
+++ src/main/java/dal/UserDalImp.java
@@ -1,101 +1,49 @@
 package dal;
 
 import dto.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.sql.*;
-import java.util.ArrayList;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 public class UserDalImp implements UserDal {
+	private static final Logger logger = LoggerFactory.getLogger(UserDalImp.class);
 
-	public static final String DB_URL = "jdbc:mysql://db4free.net:3306/lits_cv_java_2";
-	public static final String DB_USER = "java_2_user";
-	public static final String DB_PASSWORD = "java_2_password";
-	public static final String DB_COLUMN_USER_ID = "id";
-	public static final String DB_COLUMN_USER_NAME = "name";
-	public static final String DB_COLUMN_USER_EMAIL = "email";
-	public static final String DB_COLUMN_USER_ROLE_ID = "role_id";
-	public static final String DB_COLUMN_USER_PASSWORD = "password";
+	private SessionFactory sessionFactory;
 
-	public UserDalImp() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+	public void setSessionFactory(SessionFactory sf) {
+		this.sessionFactory = sf;
 	}
 
+
 	@Override
+	@Transactional
 	public List<User> readAllFromDB() {
-		Connection connection = null;
-		Statement statement = null;
-		List<User> userList = new ArrayList<>();
-		try {
-			connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			statement = connection.createStatement();
-
-			String sqlQuery = "SELECT * FROM user";
-			ResultSet resultSet = statement.executeQuery(sqlQuery);
-
-			while (resultSet.next()) {
-				Integer id = resultSet.getInt(DB_COLUMN_USER_ID);
-				String name = resultSet.getString(DB_COLUMN_USER_NAME);
-				String email = resultSet.getString(DB_COLUMN_USER_EMAIL);
-				String role_id = resultSet.getString(DB_COLUMN_USER_ROLE_ID);
-				String password = resultSet.getString(DB_COLUMN_USER_PASSWORD);
-
-				User user = new User(id, name);
-				userList.add(user);
-
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
+		Session session = this.sessionFactory.getCurrentSession();
+		List<User> userList = session.createQuery("from user").list();
 		return userList;
+
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> readFromDBById(int id) {
-		Connection connection = null;
-		Statement statement = null;
-		List<User> userList = new ArrayList<>();
-		try {
-			connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			statement = connection.createStatement();
-
-			String sqlQuery = "SELECT * FROM user WHERE ID = "+ id;
-			ResultSet resultSet = statement.executeQuery(sqlQuery);
-
-			while (resultSet.next()) {
-				Integer idFromDB = resultSet.getInt(DB_COLUMN_USER_ID);
-				String name = resultSet.getString(DB_COLUMN_USER_NAME);
-				String email = resultSet.getString(DB_COLUMN_USER_EMAIL);
-				String role_id = resultSet.getString(DB_COLUMN_USER_ROLE_ID);
-				String password = resultSet.getString(DB_COLUMN_USER_PASSWORD);
-
-				User user = new User(id, name);
-
-				return Optional.of(user);
-
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-
-
 
 		return Optional.empty();
 	}
 
 	@Override
+	@Transactional
 	public boolean createUserInToDB(User user) {
 		return false;
 	}
 
 	@Override
+	@Transactional
 	public boolean updateUser(int id, User user) {
 		return false;
 	}
--- src/main/java/dto/User.java
+++ src/main/java/dto/User.java
@@ -1,22 +1,37 @@
 package dto;
 
+import javax.persistence.*;
+
+@Table
+@Entity(name = "user")
+
 public class User {
 
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String email;
+	private int role_id;
+	private String password;
+	private int address_id;
+	private String status;
 
-	public User(Integer id, String name) {
-		this.id = id;
-		this.name = name;
+	public User() {
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", email='" + email + '\'' +
+				", role_id=" + role_id +
+				", password='" + password + '\'' +
+				", address_id=" + address_id +
+				", status='" + status + '\'' +
+				'}';
 	}
 
 	public Integer getId() {
@@ -35,12 +50,53 @@ public class User {
 		this.name = name;
 	}
 
-	@Override
-	public String toString() {
-		return "User{" +
-				"id=" + id +
-				", name='" + name + '\'' +
-				", email='" + email + '\'' +
-				'}';
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAddress_id() {
+		return address_id;
+	}
+
+	public void setAddress_id(int address_id) {
+		this.address_id = address_id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public User(Integer id, String name, String email, int role_id, String password, int address_id, String status) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.role_id = role_id;
+		this.password = password;
+		this.address_id = address_id;
+		this.status = status;
 	}
 }
