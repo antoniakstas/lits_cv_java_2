7e80237281effbfd01a81d45824321db161c93f5
Melnichuk Olexandr <alex.melnichuk90@gmail.com>
2019-07-25 21:38:45 +0300

    add change

--- src/main/java/controller/CartController.java
+++ src/main/java/controller/CartController.java
@@ -1,18 +1,15 @@
 package controller;
 
 
-import com.sun.xml.bind.v2.TODO;
 import dto.*;
 import model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import service.*;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -21,44 +18,39 @@ import java.util.Optional;
 @RequestMapping("/cart")
 class CartController {
 
-
     @Autowired
     private CartService cartService;
 
-
     @Autowired
     private OrderService orderService;
+
     @Autowired
     private PriceService priceService;
+
     @Autowired
     private UserService userService;
+
     @Autowired
     private ProductService productService;
 
     @GetMapping(path = "/read")
     public CartListResponseModel findItem(Long order_id) {
         CartListResponseModel responseModel = new CartListResponseModel();
-
         List<CartResponseModel> allCartByOrderId = cartService.findAllCartByOrderId(order_id);
         responseModel.setCartResponseModel(allCartByOrderId);
-
-
         return responseModel;
     }
-
 
     @GetMapping(path = "/list")
     public List<Cart> findAll() {
         List<Cart> allCarts = cartService.findAllCarts();
         return allCarts;
-
     }
 
     @PostMapping(value = "/add")
     public Cart createCart(@ModelAttribute("cart") Cart cart) {
 
         Optional<Cart> cartWasCreated = this.cartService.createCart(cart);
-
 
         if (cartWasCreated.isPresent()) {
             return cartWasCreated.get();
@@ -76,7 +68,6 @@ class CartController {
         if (cartWasUpdated.isPresent()) {
             return cartWasUpdated.get();
         }
-
         return null;
     }
 
@@ -84,12 +75,10 @@ class CartController {
     public void deleteLine(Long id) {
 
         Optional<Cart> optionalCart = this.cartService.deleteLine(id);
-
     }
 
     @GetMapping(path = "/listToCart")
     public ModelAndView findAll1() {
-
 
         List<Order> orderList = orderService.findAllOrder();
         List<Cart> allCarts = cartService.findAllCarts();
@@ -111,18 +100,10 @@ class CartController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
 
-
-//        System.out.println("I'm in the GET method!");
-//        OrderModel orderModel = new OrderModel(123, " user name", 1, 2,"e");
-//        OrderDalImp dal = new OrderDalImp();
-//        dal.readAllFromDB();
-
         ModelAndView modelAndView = new ModelAndView("thanksForRegistration");
         modelAndView.addObject("currentUserName", username);
         modelAndView.addObject("orderListToHtml", orderList);
         modelAndView.addObject("orderListWithCartItemsToHtml", orderListExtended);
-
-
         return modelAndView;
     }
 
@@ -130,37 +111,76 @@ class CartController {
     public ModelAndView emptyCartPage() {
         List<CartResponse> response = new ArrayList<>();
 
+        Integer summary = 0;
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
 
-
-        List<User> allUserListByName = userService.readAllFromDBByName(username);
-//        Integer getUserIdByName = allUserListByName.get(0).getId();
-        //TODO: classcastex Integer
-//        for (User userItem : allUserListByName) {
-//            userItem.getId();
-//        }
-
-        Integer idUserName = 2;
-        List<Order> orderList = orderService.findOrderById(idUserName);
+        Integer idUserName = userService.readUserIdByName(username);
+        String status = "not worked out";
+        List<Order> orderList = orderService.findOrderByUserCId(idUserName, status);
         Integer orderid = orderList.get(0).getId();
 
-        List<Cart> cartListList = cartService.readFromDBById(orderid);
-        Long priceIdFromCart = Long.valueOf(cartListList.get(0).getPrice_id());
+        List<Cart> cartListList = cartService.readFromDBByOrderId(orderid);
 
-        for (Cart cartItem: cartListList){
-            cartItem.getProduct_count();
+
+        for (Cart cartItem : cartListList) {
+            CartResponse cartResponse = new CartResponse();
+            cartResponse.setCount(cartItem.getProduct_count());
+            Long priceItemByCart = Long.valueOf(cartItem.getPrice_id());
+
+            List<Price> allPriceByProductId =
+                    priceService.readAllFromDBByPriceId(priceItemByCart);
+
+            for (Price priceItem : allPriceByProductId) {
+                cartResponse.setPrice((int) (priceItem.getValue() * priceItem.getMult()));
+                cartResponse.setDeliverydays(priceItem.getDeliverydays());
+                Long productItemByPrice = priceItem.getProductId();
+                Product productByProductId =
+                        productService.findById(productItemByPrice);
+                cartResponse.setIndex(productByProductId.getIndex());
+                cartResponse.setName(productByProductId.getName());
+                cartResponse.setManufacturer(productByProductId.getManufacturer());
+            }
+            response.add(cartResponse);
+            summary = (cartResponse.getCount()*cartResponse.getPrice())+summary;
         }
-        List<Price> priceList = priceService.readAllFromDBByPriceId(priceIdFromCart);
+        ModelAndView modelAndView = new ModelAndView("cartByUserName");
 
-//        Integer idUserName = SELECT id FROM lits_cv_java_2.user where name = 'username';
+        modelAndView.addObject("response", response);
+        modelAndView.addObject("response2", summary);
 
-        ModelAndView modelAndView = new ModelAndView("cartIsEmpty");
+        return modelAndView;
+    }
 
+    @GetMapping(path = "/addToCart")
+    public ModelAndView addProductToCart(Integer priceId) {
+        ModelAndView modelAndView = new ModelAndView("cart");
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        Integer userId = userService.readUserIdByName(username);
+
+        String status = "not worked out";
+
+        List<Order> orderList = orderService.findOrderByUserCId(userId, status);
+
+        if (orderList.isEmpty()) {
+            Order order = new Order(1, status, 1, userId);
+            orderService.createOrderInToDB(order);
+            Cart cart1 = new Cart(null, order.getId(), 1, priceId);
+            cartService.createCart(cart1);
+        } else {
+            Order curentOrder = orderList.get(0);
+            curentOrder.getId();
+            Cart cart = new Cart(null, curentOrder.getId(), 1, priceId);
+            cartService.createCart(cart);
+        }
 
         return modelAndView;
 
-
     }
+
 
 }
