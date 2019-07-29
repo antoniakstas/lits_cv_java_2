15d7fb44f80f36abe116c61b7d3e5b7a4ac38689
Ilia <ilia97ap76@gmail.com>
2019-07-29 11:41:02 +0300

    add change in UserModel

--- src/main/java/model/UserModel.java
+++ src/main/java/model/UserModel.java
@@ -4,6 +4,25 @@ public class UserModel {
 
 	private Integer id;
 	private String name;
+	private String email;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	private String url;
 
 	public UserModel(Integer id, String name, String url) {
